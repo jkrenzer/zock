794a82f21d51f80527f2385620e5fcb5a34a21dd
Jörn Krenzer <joern.krenzer@gmx.de>
2021-12-14 23:24:21 +0100

    Changed db-interface

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,3 @@
 import * as indexedDb from "./indexedDb";
 
-export function getDb(dbType: string) {
-  if (dbType == 'indexedDb') {
-    if (indexedDb.checkCapability()) {
-      return indexedDb
-    }
-    else {
-      console.log("Your browser doesn't support a stable version of IndexedDB!");
-    }
-  }
-  return null;
-}
+export {indexedDb};
