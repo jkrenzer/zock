8620fa8b338d28a1a6f50999ce34bd1430c41f89
Jörn Krenzer <joern.krenzer@gmx.de>
2021-12-14 23:41:19 +0100

    Got rid of an unused function

--- src/flow/createConfig.ts
+++ src/flow/createConfig.ts
@@ -1,11 +1,6 @@
 import {Config} from "../data";
 import {createSigningKeyPair, createEncryptionKeyPair} from "../crypto";
 
-function getRandomInt(max: number) {
-
-  return Math.floor(Math.random() * max);
-}
-
 export async function createConfig() : Promise<Config> {
   let signingKeypair = await createSigningKeyPair();
   let encryptionKeypair = await createEncryptionKeyPair();
